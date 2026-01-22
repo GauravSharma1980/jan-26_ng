f686c6ca2f5ea4dce39395d19abc7dc4e1f30d6d
Sumit Khandelwal <synergy2411@outlook.com>
2026-01-22 10:15:43 +0530

    property binding syntax added

--- frontend-two/src/app/components/users/users.component.ts
+++ frontend-two/src/app/components/users/users.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
 export class UsersComponent {
-
+  user = {
+    firstName: 'tim b.',
+    lastName: 'lee',
+    dob: new Date('Dec 21, 1965'),
+    income: 40000,
+    isWorking: true,
+    company: 'World Wide Web',
+    avatar: './assets/images/tim.jpg',
+  };
 }
